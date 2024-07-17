af1d6b449793191836a5ca404680751d6e574c26
mumtozBEK1234 <email manzilingiz>
2024-07-17 21:51:01 +0300

    2 dars

---
+++ my.js
@@ -0,0 +1,41 @@
+// 1-masal 
+
+// let num = +prompt("sonnin kiritin");
+
+// if (num > 0) {
+//     num++;
+// } else{
+//     num -= 2;
+// }
+
+// console.log(num);
+
+// 3-masala
+
+// let num = +prompt("sonnin kiritin");
+
+// if (num > 0) {
+//     num++;
+// } else if (num == 0) {
+//     num = 10;
+// } else {
+//     num -= 2;
+// }
+// console.log(num);
+
+// 4-masala 
+
+// let numbers = [1, -2, 3, -4, 5, -6, 0];
+// let num  = +prompt("1 sonni kiritin");
+// let num2 = +prompt("2 sonni kiritin");
+  
+// if (number !== 0) {
+//         number > 0 ? num : num2  ;
+//     }
+    
+//     console.log("Musbat sonlar: " + num);
+// console.log("Manfiy sonlar: " + num2);
+
+
+
+
